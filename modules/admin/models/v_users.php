ffd3ce30b9d1834199b7e378825618ad752ca481
Serhiy <novoseletskiyserhiy@gmail.com>
2015-06-21 21:50:47 +0300

    Зробив випускників

---
+++ modules/admin/models/v_users.php
@@ -0,0 +1,10 @@
+<?php
+class v_users {
+    public $_user_id;
+    public $login;
+    public $avatar;
+    public $group_id;
+    public $first_name;
+    public $last_name;
+    public $sur_name;
+}
