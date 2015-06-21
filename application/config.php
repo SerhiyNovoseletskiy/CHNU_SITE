35bdc7795069ef0efef886343bc4efd16a394266
Serhiy <novoseletskiyserhiy@gmail.com>
2015-06-21 19:18:58 +0300

    Змінив назву сайту

--- application/config.php
+++ application/config.php
@@ -4,14 +4,21 @@ define("DB_USER", 'root');
 define("DB_PASS", '');
 define("DB", 'chnu');
 
-define("SITE_NAME", 'ЧНУ');
 define("APP_VERSION", 0.2);
 define("DEFAULT_LANGUAGE", 'ua');
 
-if (!isset($_COOKIE['language']))
+if (!isset($_COOKIE['language'])) {
     define('LANGUAGE', 'ua');
-else
+
+} else {
     define('LANGUAGE', $_COOKIE['language']);
 
+}
+
+if (LANGUAGE == 'ua')
+    define('SITE_NAME', '«Об’єднання Випускників та Друзів Чернівецького національного університету імені Юрія Федьковича»');
+else
+    define('SITE_NAME', '"Association of Alumni and Friends of Yuriy Fedkovych Chernivtsi National University"');
+
 define('TEMPLATE', 'chnu');
-define('DEBUG',true);
+define('DEBUG', true);
