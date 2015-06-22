d995f9b1c3df09439e3f4ac82c5663979d3e95db
Alex Plishkin <sania_pl@mail.ru>
2015-06-22 22:13:00 +0300

    First step for chat

---
+++ modules/messages/index.php
@@ -0,0 +1,16 @@
+<?php
+
+class c_messages extends Controller
+{
+    function _index()
+    {
+        if (!empty($this->url[2])) {
+        } else {
+
+        }
+
+        $this->view = 'messages';
+    }
+
+
+}
