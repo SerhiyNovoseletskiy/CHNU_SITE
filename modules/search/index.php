dd0856edaeac9fef5cd70478457567f4af9c83b5
Serhiy <novoseletskiyserhiy@gmail.com>
2015-06-23 20:59:44 +0300

    dbajh

--- modules/search/index.php
+++ modules/search/index.php
@@ -2,30 +2,30 @@
 class c_search extends Controller{
     public function _index()
     {
-        $this->meta['title'] = SEARCH;
-
         global $model;
         LoadModel('admin', 'v_users');
-        LoadModel('account','department');
-        $this->data['users'] = $model->getByParam(
-            new v_users(),
-            array(
-                'group_id' => 2
-            )
-        );
 
-        $this->data['departments'] = $model->getAll(
-            new department()
-        );
+        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $this->template = false;
 
+
+            LoadView('search', 'search', $this->data);
+        } else {
+            $this->meta['title'] = SEARCH;
+
+            LoadModel('account','department');
+            $this->data['users'] = $model->getByParam(
+                new v_users(),
+                array(
+                    'group_id' => 2
+                )
+            );
+
+            $this->data['departments'] = $model->getAll(
+                new department()
+            );
+        }
 
     }
 
-    public function _search()
-    {
-        $this->template = false;
-        $this->data = 'Hello';
-
-        LoadView('search', 'search', $this->data);
-    }
 }
