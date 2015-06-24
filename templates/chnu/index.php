7c6b004eab95e7f3dd195f50844d5bf35757189d
Serhiy <novoseletskiyserhiy@gmail.com>
2015-06-24 19:36:56 +0300

    Змінив header

--- templates/chnu/index.php
+++ templates/chnu/index.php
@@ -15,14 +15,15 @@ $graduates = LoadPlugin('Graduates');
 </head>
 <body>
 <div class="container">
-    <div class="header-image"></div>
+    <div class="header-image" style = 'background-image: url("/templates/chnu/images/<?=LANGUAGE?>.header.jpg")'></div>
+    <div class="clearfix"></div>
     <nav class="navbar">
         <div class="container-fluid">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
                         data-target="#bs-example-navbar-collapse-1">
-                    <span class="glyphicon glyphicon-collapse-down"></span>
+                    <img src="/templates/chnu/images/menu.png">
                 </button>
             </div>
 
