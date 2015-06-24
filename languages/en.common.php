66ef86fbdaa6233781c6066896c81fe14a8f09d3
Vova Perebykisvkiy <vova235@gmail.com>
2015-06-24 12:50:13 +0300

    Changes in languages. Add validation for registration view.

--- languages/en.common.php
+++ languages/en.common.php
@@ -11,16 +11,30 @@ define('GRADUATE', 'Graduate');
 define('PROFILE', 'My profile');
 define('MESSAGES', 'My messages');
 define('LAST', 'Latest');
+
 define('LOGIN', 'Username');
 define('PASSWORD', 'Password');
 define('EMAIL', 'Email');
+define('EMAIL_NOT_MATCH', 'Incorrect email');
+define('EMAIL_HINT', 'Electronic mail');
+define('EMAIL_TITLE', 'Example: testemail@email.uk');
 define('SUR_NAME', 'Surname');
+define('SUR_NAME_NOT_MATCH', 'Incorrect surname');
+define('SUR_NAME_TITLE', 'Example: Norris');
 define('FIRST_NAME', 'Name');
+define('FIRST_NAME_NOT_MATCH', 'Incorrect name');
+define('FIRST_NAME_TITLE', 'Chuck');
 define('MIDDLE_NAME', 'Middle name');
+define('MIDDLE_NAME_NOT_MATCH', 'Incorrect middle name');
+define('MIDDLE_NAME_TITLE', 'Volodymyrovych');
 define('BIRTHDAY', 'Birthday');
 define('TELEPHONE', 'Mobile number');
+define('TELEPHONE_NOT_MATCH', 'Incorrect phone');
+define('TELEPHONE_TITLE', 'Example: +380505145254');
 define('ENTRY_YEAR', 'Entry year');
+define('ENTRY_YEAR_TITLE', 'Example: 1995');
 define('GRADUATION_YEAR', 'Graduation year');
+define('GRADUATION_YEAR_TITLE', 'Example: 2000');
 define('DEPARTMENT', 'Department(institution)');
 define('SPECIALITY', 'Speciality');
 define('TEACHING_FORM', 'Form of teaching');
@@ -28,3 +42,6 @@ define('EDUCATION_QUALIFICATION', 'EQ');
 define('EDUCATION_QUALIFICATION_HINT', 'Education qualification');
 define('ADDITIONAL_INFO', 'Additional information');
 define('FIELDS_HINT', 'Required field');
+
+
+
