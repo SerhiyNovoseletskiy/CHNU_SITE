226f0b31b17e2da7c96c626eb8a36d916530944b
Vova Perebykisvkiy <vova235@gmail.com>
2015-06-24 13:39:20 +0300

    Restructure of files.

--- languages/ua.common.php
+++ languages/ua.common.php
@@ -12,37 +12,3 @@ define('PROFILE', 'Мій профіль');
 define('MESSAGES', 'Мої повідомлення');
 define('LAST', 'Останні записи');
 define('DISCUSSIONS', 'Обговорення');
-
-
-
-define('LOGIN', 'Псевдонім');
-define('PASSWORD', 'Пароль');
-define('EMAIL', 'Е-пошта');
-define('EMAIL_NOT_MATCH', 'Неправильно записана E-пошта');
-define('EMAIL_HINT', 'Електронна пошта');
-define('EMAIL_TITLE', 'Приклад: testemail@email.ua');
-define('SUR_NAME', 'Прізвище');
-define('SUR_NAME_NOT_MATCH', 'Неправильно записано прізвище');
-define('SUR_NAME_TITLE', 'Приклад: Фокін');
-define('FIRST_NAME', 'Ім'."'".'я');
-define('FIRST_NAME_NOT_MATCH', 'Неправильно записано ім' . "'" . 'я');
-define('FIRST_NAME_TITLE', 'Сашко');
-define('MIDDLE_NAME', 'По батькові');
-define('MIDDLE_NAME_NOT_MATCH', 'Неправильно записано по батькові');
-define('MIDDLE_NAME_TITLE', 'Адольфович');
-define('BIRTHDAY', 'Дата народження');
-define('TELEPHONE', 'Мобільний телефон');
-define('TELEPHONE_NOT_MATCH', 'Неправильно записано номер телефону');
-define('TELEPHONE_TITLE', 'Приклад: +380505145254');
-define('ENTRY_YEAR', 'Рік вступу');
-define('ENTRY_YEAR_TITLE', 'Приклад: 2010');
-define('GRADUATION_YEAR', 'Рік випуску');
-define('GRADUATION_YEAR_TITLE', 'Приклад: 2016');
-define('DEPARTMENT', 'Факультет(інститут)');
-define('SPECIALITY', 'Спеціальність');
-define('TEACHING_FORM', 'Форма навчання');
-define('EDUCATION_QUALIFICATION', 'ОКР');
-define('EDUCATION_QUALIFICATION_HINT', 'Освітньо кваліфікаційний рівень');
-define('ADDITIONAL_INFO', 'Додаткова інформація');
-define('FIELDS_HINT', '* - обов' . "'" . 'язкове поле');
-
