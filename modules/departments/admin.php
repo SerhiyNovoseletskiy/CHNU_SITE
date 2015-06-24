fe3fed6824476b753495d8b32c23c0c11b1e34b5
Serhiy <novoseletskiyserhiy@gmail.com>
2015-06-24 15:17:07 +0300

    Зробив CRUD факультетів в адмінці

--- modules/departments/admin.php
+++ modules/departments/admin.php
@@ -1,12 +1,15 @@
 <?php
-class admin_departments extends Controller {
 
-    function _index() {
+class admin_departments extends Controller
+{
+
+    function _index()
+    {
         global $model;
         LoadModel('account', 'department');
         LoadModel('admin', 'menu');
         $this->view = 'index';
-        $this->meta['title'] = DEPARTMENTS;
+        $this->meta['title'] = 'Факультети';
         $this->data = $model->getAll(new department());
 
         $menu = new menu();
@@ -18,23 +21,75 @@ class admin_departments extends Controller {
         $this->menu = array($menu);
     }
 
-    function _insert() {
+    function _add()
+    {
+        $this->meta['title'] = 'Новий факультет';
+        $this->view = 'add';
 
+        LoadModel('admin', 'menu');
+        $menu = new menu();
+        $menu->tag = 'a';
+        $menu->href = MODULE_URL;
+        $menu->value = '<span class="glyphicon glyphicon-arrow-left"></span>';
+        $menu->class = 'btn btn-danger';
+
+        $this->menu = array($menu);
     }
 
-    function _create() {
+    function _save()
+    {
         $this->template = false;
+        global $model;
+        LoadModel('account', 'department');
+
+        $dep = new department();
+        $dep->name = $_POST['name'];
+        $model->save($dep);
+
+        header("Location: " . MODULE_URL);
     }
 
-    function _edit() {
+    function _edit()
+    {
+        global $model;
+        LoadModel('account', 'department');
 
+        $this->data = $model->getById(new department(), $this->url[5]);
+        $this->meta['title'] = $this->data->name;
+
+        $this->view = 'edit';
+
+        LoadModel('admin', 'menu');
+        $menu = new menu();
+        $menu->tag = 'a';
+        $menu->href = MODULE_URL;
+        $menu->value = '<span class="glyphicon glyphicon-arrow-left"></span>';
+        $menu->class = 'btn btn-danger';
+
+        $this->menu = array($menu);
     }
 
-    function _update() {
+    function _update()
+    {
         $this->template = false;
+        global $model;
+        LoadModel('account', 'department');
+
+        $this->data = $model->getById(new department(), $this->url[5]);
+
+        $this->data->name = $_POST['name'];
+        $model->update($this->data);
+        header("Location: " . MODULE_URL . '/edit/' . $this->data->_id);
     }
 
-    function _delete() {
+    function _delete()
+    {
         $this->template = false;
+
+        global $model;
+        LoadModel('account', 'department');
+
+        $model->delete($model->getById(new department(), $this->url[5]));
+        header("Location: " . MODULE_URL);
     }
 }
